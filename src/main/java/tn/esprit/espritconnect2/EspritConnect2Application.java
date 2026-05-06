730d27a34b219ae6e73e2fed3f45f3423b6accc9
Rahma Ayari <rahmaayari272@gmail.com>
2026-05-06 20:52:50 +0200

    first commit

---
+++ src/main/java/tn/esprit/espritconnect2/EspritConnect2Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.espritconnect2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspritConnect2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspritConnect2Application.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/espritconnect2/EspritConnect2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.espritconnect2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspritConnect2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
