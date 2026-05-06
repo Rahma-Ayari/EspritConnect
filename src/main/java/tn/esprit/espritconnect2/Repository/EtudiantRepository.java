c8e49b24d7f2e3509570449a2d55e8ea9168e49f
Rahma Ayari <rahmaayari272@gmail.com>
2026-05-07 00:09:46 +0200

    fix existsByEmail

--- src/main/java/tn/esprit/espritconnect2/Repository/EtudiantRepository.java
+++ src/main/java/tn/esprit/espritconnect2/Repository/EtudiantRepository.java
@@ -6,9 +6,9 @@ import tn.esprit.espritconnect2.Entitie.Etudiant;
 import java.util.Optional;
 
 public interface EtudiantRepository extends JpaRepository<Etudiant,Long> {
-//    // Vérifier si un email existe déjà (utile pour la validation)
-//    boolean existsByEmail(String email);
-//
-//    // Trouver par email (utile pour Spring Security plus tard)
-//    Optional<Etudiant> findByEmail(String email);
+    // Vérifier si un email existe déjà (utile pour la validation)
+    boolean existsByEmail(String email);
+
+    // Trouver par email (utile pour Spring Security plus tard)
+    Optional<Etudiant> findByEmail(String email);
 }
