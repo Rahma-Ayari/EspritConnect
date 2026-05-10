96d6c4a130290b3f975c95c8e4b2690598180683
bennasrsafa <safabennasr14@gmail.com>
2026-05-10 17:39:26 +0100

    Fix: add existsByEmail to AlumniRepository

--- src/main/java/tn/esprit/espritconnect2/Repository/AlumniRepository.java
+++ src/main/java/tn/esprit/espritconnect2/Repository/AlumniRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tn.esprit.espritconnect2.Entitie.Alumni;
 
 public interface AlumniRepository extends JpaRepository<Alumni, Long> {
+    boolean existsByEmail(String email);
 }
